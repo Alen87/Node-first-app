c518bb110b470e94848c25b170b5d1e76619f200
Alen Boric <boricalen@live.com>
2022-11-13 00:53:54 +0100

    server

--- first-app.js
+++ first-app.js
@@ -1,1 +1,20 @@
-console.log('Hello from Node.js');
+const http = require('http');
+
+const server = http.createServer((req, res)=>{
+    const url = req.url;
+    if(url === '/'){
+    res.write('<html>');
+    res.write('<head><title>Enter Message</title></head>');
+    res.write('<body><form><input type="text"><button type="submit"></button></form></body>')
+    res.write('</html>');
+    res.end(); 
+    }
+    res.setHeader('Content-Type','text/html');
+    res.write('<html>');
+    res.write('<head><title>My First Page</title></head>');
+    res.write('<body><h1>Hello from my Node.js Server!</h1></body>')
+    res.write('</html>');
+    res.end();
+});
+
+server.listen(3000);
